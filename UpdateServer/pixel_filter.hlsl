839ac5cc4ce41f8719587cf54cf2f84b6a8a35c5
Dario <dariosamo@gmail.com>
2022-06-24 07:33:04 -0300

    Setting up update server.

---
+++ UpdateServer/pixel_filter.hlsl
@@ -0,0 +1,17 @@
+SamplerState s_Source : register(s0);
+SamplerState s_Source1 : register(s1);
+Texture2D t_Source : register(t0);
+Texture2D t_Source1 : register(t1);
+
+cbuffer Globals : register(b0)
+{
+   float4 textureSize;
+   float4 viewSize;
+   float4 filterParam;
+};
+
+float4 main(float4 svPos : SV_POSITION, float2 tc : TEXCOORD) : SV_TARGET
+{
+	uint2 tCoord = floor(tc * textureSize.xy);
+	return t_Source.Load(uint3(tCoord, 0));
+}
